0e8b0c69c356d8d03227527215a2e489221924d6
Danila <danil_malkin@list.ru>
2023-10-09 17:21:36 +0300

    Added project structure

---
+++ ios_proj/ios_proj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_proj
+//
+//  Created by Danila on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
