65757284575d33a9ece1d8a6f210d7f076807d16
Danila <danil_malkin@list.ru>
2023-11-19 00:24:57 +0300

    added BeerTableView

---
+++ ios_proj/ios_proj/ShopTable/ShopTableView.swift
@@ -0,0 +1,62 @@
+//
+//  ShopTableView.swift
+//  ios_proj
+//
+//  Created by Danila on 18.11.2023.
+//
+
+import UIKit
+
+final class ShopTableView: UIView{
+    
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+       
+        return tableView
+    }()
+    
+    private lazy var spinnerView = UIActivityIndicatorView(style: .large)
+    
+    init(){
+        super.init(frame: .zero)
+        self.backgroundColor = .white
+        addSubviews()
+        makeConstraints()
+        spinnerView.startAnimating()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+private extension ShopTableView{
+    func addSubviews(){
+        [tableView, spinnerView].forEach{
+            self.addSubview($0)
+        }
+    }
+    func makeConstraints(){
+        [tableView, spinnerView].forEach{
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            
+            spinnerView.centerXAnchor.constraint(equalTo: tableView.centerXAnchor),
+            spinnerView.centerYAnchor.constraint(equalTo: tableView.centerYAnchor)
+        ])
+        
+
+    
+        
+    }
+}
--- ios_proj/ios_proj/ShopTable/ViewController.swift
+++ ios_proj/ios_proj/ShopTable/ViewController.swift
@@ -9,52 +9,16 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.backgroundColor = .systemGray
-        //tableView.dataSource = self
-        return tableView
-    }()
-    
-    
-   
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
-        
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-        
-        view.addSubview(activityIndicator)
-
-        NSLayoutConstraint.activate([
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-
-        activityIndicator.startAnimating()
-        
-        
-        
-       
-    
-        
+        view.backgroundColor = .white
         
     }
-
-
 }
 
 
 
-extension UIImageView {
+/*extension UIImageView {
     func load(url: URL) {
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
@@ -67,3 +31,4 @@ extension UIImageView {
         }
     }
 }
+*/
